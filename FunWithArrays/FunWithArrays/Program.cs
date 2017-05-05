368cb4a6cacaa79a9d6e327a4815ef38ff52a378
Steff McKee <steffmckee@outlook.com>
2017-05-05 08:47:50 -0600

    DeclareImplicitArrays

--- FunWithArrays/FunWithArrays/Program.cs
+++ FunWithArrays/FunWithArrays/Program.cs
@@ -10,8 +10,9 @@ namespace FunWithArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Fun with Arrays *****");
+            Console.WriteLine("***** Fun with Arrays *****\n");
             SimpleArrays();
+            DeclareImplicitArrays();
             Console.ReadLine();
         }
 
@@ -30,6 +31,22 @@ namespace FunWithArrays
             }
 
             string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
+        }
+
+        static void DeclareImplicitArrays()
+        {
+            Console.WriteLine("=> Implicit Array Initialization.");
+
+            var a = new[] { 1, 10, 100, 1000 };
+            Console.WriteLine("a is a: {0}", a.ToString());
+
+            var b = new[] { 1, 1.5, 2, 2.5 };
+            Console.WriteLine("b is b: {0}", b.ToString());
+
+            var c = new[] { "hello", null, "world" };
+            Console.WriteLine("c is c: {0}", c.ToString());
+
             Console.WriteLine();
         }
     }
