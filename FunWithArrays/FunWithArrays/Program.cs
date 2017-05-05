ef6ab38a42daf3f6632392d73f3973dd77771397
Steff McKee <steffmckee@outlook.com>
2017-05-05 08:38:05 -0600

    SimpleArray

--- FunWithArrays/FunWithArrays/Program.cs
+++ FunWithArrays/FunWithArrays/Program.cs
@@ -10,6 +10,27 @@ namespace FunWithArrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Arrays *****");
+            SimpleArrays();
+            Console.ReadLine();
+        }
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            foreach(int i in myInts)
+            {
+                Console.WriteLine(i);
+            }
+
+            string[] booksOnDotNet = new string[100];
+            Console.WriteLine();
         }
     }
 }
